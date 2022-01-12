1c669bf1c6e7e647f036e74746804666d6a5fb9d
rohit553-ai <68155913+rohit553-ai@users.noreply.github.com>
2022-01-12 22:09:56 +0545

    working on user verification and handled error for invalid api endpoints

--- src/constants/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.TEXT = void 0;
-exports.TEXT = {
-    SERVER_STATUS: "🚀 Server running on port http://localhost:",
-    DATABASE_STATUS: "🗳  database connected",
-};
